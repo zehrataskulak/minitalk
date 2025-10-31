8dcab3bcf08d7487ae91fa686146843b20f089c2
Zehra Taskulak <ztaskula@k1m17s08.42istanbul.com.tr>
2025-10-31 12:26:09 +0300

    norms corrected

--- client.c
+++ client.c
@@ -6,47 +6,47 @@
 /*   By: zzehra <zzehra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 13:38:23 by zzehra            #+#    #+#             */
-/*   Updated: 2025/10/28 19:34:06 by zzehra           ###   ########.fr       */
+/*   Updated: 2025/10/31 12:17:33 by ztaskula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <signal.h>
 
-void    send_message(int pid, char *argv)
+void	send_message(int pid, char *argv)
 {
-    int len;
-    int i;
-    int j;
-    unsigned int bit;
+	int				len;
+	int				i;
+	int				j;
+	unsigned int	bit;
 
-    i = 0;
-    len = ft_strlen(argv);
-    while(i < len)
-    {
-        j = 0;
-        while(j < 8)
-        {
-            bit = (argv[i] >> j) & 1;
-            if (bit == 0)
-                kill(pid, SIGUSR1);
-            else if (bit == 1)
-                kill(pid, SIGUSR2);
-            j++;
-            usleep(100);
-        }
-        i++;
-    }
+	i = 0;
+	len = ft_strlen(argv);
+	while (i < len)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			bit = (argv[i] >> j) & 1;
+			if (bit == 0)
+				kill(pid, SIGUSR1);
+			else if (bit == 1)
+				kill(pid, SIGUSR2);
+			j++;
+			usleep(500);
+		}
+		i++;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int pid;
+	int	pid;
 
-    if (argc != 3)
-        exit (0);    
-    pid = ft_atoi(argv[1]);
-    send_message(pid, argv[2]);
-    kill(pid, '\0');
-    return 0;
+	if (argc != 3)
+		exit (0);
+	pid = ft_atoi(argv[1]);
+	send_message(pid, argv[2]);
+	kill(pid, '\0');
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,48 +6,45 @@
 /*   By: zzehra <zzehra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 13:38:16 by zzehra            #+#    #+#             */
-/*   Updated: 2025/10/28 20:21:01 by zzehra           ###   ########.fr       */
+/*   Updated: 2025/10/31 12:25:49 by ztaskula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf/ft_printf.h"
 #include <signal.h>
 
-void find_ascii(int sig)
+void	find_ascii(int sig)
 {
-    static int indx;
-    int indx_tmp;
-    static unsigned int ascii;
-    unsigned int ascii_tmp;
-    
-    indx_tmp = indx;
-    ascii_tmp = 1;
-    if (sig == SIGUSR1)
-    {
-        indx++;
-    }
-    else if (sig == SIGUSR2)
-    {
-        while(indx_tmp--)
-            ascii_tmp *= 2;
-        ascii += ascii_tmp;
-        indx++;
-    }
-    if(indx == 8)
-    {
-        write(1, &ascii, 1);
-        ascii = 0;
-        indx = 0;  
-    }
+	int						indx_tmp;
+	static int				indx;
+	unsigned int			ascii_tmp;
+	static unsigned int		ascii;
+
+	indx_tmp = indx;
+	ascii_tmp = 1;
+	if (sig == SIGUSR1)
+		indx++;
+	else if (sig == SIGUSR2)
+	{
+		while (indx_tmp--)
+			ascii_tmp *= 2;
+		ascii += ascii_tmp;
+		indx++;
+	}
+	if (indx == 8)
+	{
+		write(1, &ascii, 1);
+		ascii = 0;
+		indx = 0;
+	}
 }
 
-int main()
+int	main(void)
 {
-    ft_printf("%d\n", getpid());
-    
-    signal(SIGUSR1, find_ascii);
-    signal(SIGUSR2, find_ascii);
-    while (1)
-        pause();
-    return 0;
+	ft_printf("%d\n", getpid());
+	signal(SIGUSR1, find_ascii);
+	signal(SIGUSR2, find_ascii);
+	while (1)
+		pause();
+	return (0);
 }

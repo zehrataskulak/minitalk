1acbe10bde9972ad66a785c07f627cc1e812a4cb
zehrataskulak <230504001@st.atlas.edu.tr>
2025-11-04 00:11:24 +0300

    signal() function has been replaced with sigaction() function

--- client.c
+++ client.c
@@ -6,12 +6,20 @@
 /*   By: zzehra <zzehra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 13:38:23 by zzehra            #+#    #+#             */
-/*   Updated: 2025/10/31 12:17:33 by ztaskula         ###   ########.fr       */
+/*   Updated: 2025/11/03 23:32:17 by zzehra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <signal.h>
+
+static volatile int	g_ack_received = 0;
+
+void	check_ack(int sig)
+{
+	if (sig == SIGUSR1)
+		g_ack_received = 1;
+}
 
 void	send_message(int pid, char *argv)
 {
@@ -27,13 +35,16 @@ void	send_message(int pid, char *argv)
 		j = 0;
 		while (j < 8)
 		{
+			g_ack_received = 0;
 			bit = (argv[i] >> j) & 1;
 			if (bit == 0)
 				kill(pid, SIGUSR1);
 			else if (bit == 1)
 				kill(pid, SIGUSR2);
+			usleep(100);
+			while (!g_ack_received)
+				pause();
 			j++;
-			usleep(500);
 		}
 		i++;
 	}
@@ -45,8 +56,8 @@ int	main(int argc, char **argv)
 
 	if (argc != 3)
 		exit (0);
+	signal(SIGUSR1, check_ack);
 	pid = ft_atoi(argv[1]);
 	send_message(pid, argv[2]);
-	kill(pid, '\0');
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,20 +6,21 @@
 /*   By: zzehra <zzehra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 13:38:16 by zzehra            #+#    #+#             */
-/*   Updated: 2025/10/31 12:25:49 by ztaskula         ###   ########.fr       */
+/*   Updated: 2025/11/03 23:35:31 by zzehra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf/ft_printf.h"
 #include <signal.h>
+#include "ft_printf/ft_printf.h"
 
-void	find_ascii(int sig)
+void	find_ascii(int sig, siginfo_t *info, void *context)
 {
-	int						indx_tmp;
-	static int				indx;
-	unsigned int			ascii_tmp;
-	static unsigned int		ascii;
+	int					indx_tmp;
+	static int			indx;
+	unsigned int		ascii_tmp;
+	static unsigned int	ascii;
 
+	(void)context;
 	indx_tmp = indx;
 	ascii_tmp = 1;
 	if (sig == SIGUSR1)
@@ -37,13 +38,19 @@ void	find_ascii(int sig)
 		ascii = 0;
 		indx = 0;
 	}
+	kill(info->si_pid, SIGUSR1);
 }
 
 int	main(void)
 {
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = find_ascii;
+	sa.sa_flags = SA_SIGINFO;
 	ft_printf("%d\n", getpid());
-	signal(SIGUSR1, find_ascii);
-	signal(SIGUSR2, find_ascii);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 		pause();
 	return (0);
